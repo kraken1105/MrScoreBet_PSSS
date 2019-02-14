649e7d9ef33bad2fa2d5ce2a9fe41bfff736c122
'kraken1105' <'piccirillo.pierpaolo95@gmail.com'>
2019-02-14 14:52:00 +0100

    enzucc

---
+++ MrScoreBet_PSSS/src/utils/exceptions/EsitoAlreadyInsertedException.java
@@ -0,0 +1,9 @@
+package utils.exceptions;
+
+public class EsitoAlreadyInsertedException extends Exception {
+
+	private static final long serialVersionUID = 6220211565040065697L;
+	
+	public EsitoAlreadyInsertedException(String msg) {super(msg);}
+
+}
---
+++ MrScoreBet_PSSS/src/utils/exceptions/EsitoNotInsertedException.java
@@ -0,0 +1,9 @@
+package utils.exceptions;
+
+public class EsitoNotInsertedException extends Exception {
+
+	private static final long serialVersionUID = -6688021111918055331L;
+	
+	public EsitoNotInsertedException(String msg) {super(msg);}
+
+}
